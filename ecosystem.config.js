94933867b20af77b2149f5f51ea5976a7fa84d86
Ubuntu <ubuntu@ip-172-31-7-253.us-east-2.compute.internal>
2019-09-30 04:34:06 +0000

    Setup PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'cmdscape',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-18-224-43-8.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/personal.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:theblackathena/cmdscape.git',
+      path: '/home/ubuntu/personal/cmdscape',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
