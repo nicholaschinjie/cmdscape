a8fed4a42340e9a59723b67f3414f08965d79bf0
Ubuntu <ubuntu@ip-172-31-7-253.us-east-2.compute.internal>
2019-09-30 05:08:54 +0000

    UI code

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('HEY!')
+  res.send('HOST UI CODE HERE')
 })
 
 app.listen(3000, () => console.log('Server running on port 3000'))
