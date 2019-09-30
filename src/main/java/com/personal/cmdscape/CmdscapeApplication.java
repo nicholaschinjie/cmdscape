64cb08a577ee84e219e9b2e06d2d9abd47e63886
theblackathena <nicholaschin20@gmail.com>
2019-09-29 18:35:41 -0700

    Chatroom application

---
+++ src/main/java/com/personal/cmdscape/CmdscapeApplication.java
@@ -0,0 +1,13 @@
+package com.personal.cmdscape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmdscapeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmdscapeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/cmdscape/CmdscapeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.personal.cmdscape;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CmdscapeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
